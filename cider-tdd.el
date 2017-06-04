c450dd162a7c3acb62f340779a1dd76b296acae8
Andrej Lamov <andrej.lamov@seal-software.com>
2017-06-04 16:14:16 +0200

    Save buffer before refresh

--- cider-tdd.el
+++ cider-tdd.el
@@ -1,4 +1,4 @@
-(defun cider-refresh-hook (response log-buffer)
+(defun my-cider-refresh-hook (response log-buffer)
   (nrepl-dbind-response response (out err reloading status error error-ns after before)
     (if (equal '("ok") status)
         (progn
@@ -6,4 +6,5 @@
           (cider-test-run-project-tests)
           ))))
 
-(advice-add 'cider-refresh--handle-response :after #'cider-refresh-hook)
+(advice-add 'cider-refresh--handle-response :after #'my-cider-refresh-hook)
+(advice-add 'cider-refresh :before 'save-buffer)
