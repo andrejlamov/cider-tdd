a450c58dca319c210d6ccbadeaf86ede23a47ec9
Andrej Lamov <andrej.lamov@seal-software.com>
2017-06-04 15:54:56 +0200

    Use refresh in combination with (disable-unload!) instead

--- cider-tdd.el
+++ cider-tdd.el
@@ -1,65 +1,9 @@
-(setq nr-of-loaded-files  0)
-(setq nr-of-files-to-load 0)
+(defun cider-refresh-hook (response log-buffer)
+  (nrepl-dbind-response response (out err reloading status error error-ns after before)
+    (if (equal '("ok") status)
+        (progn
+          (cider-sync-request:ns-load-all)
+          (cider-test-run-project-tests)
+          ))))
 
-(defun reset-counters ()
-  (setq nr-of-loaded-files  0)
-  (setq nr-of-files-to-load 0))
-
-(defun all-files-are-loaded ()
-  (and (eq nr-of-files-to-load
-           nr-of-loaded-files)
-       (not (eq 0 nr-of-files-to-load))))
-
-(defun maybe-run-tests ()
-  (incf nr-of-loaded-files)
-  (when (all-files-are-loaded)
-    (cider-test-run-project-tests)))
-
-(defun construct-find-command (src-folder test-folder)
-  (format "find %s %s -type f ! -name '.#*' ! -name '*.cljs'"
-          (concat (projectile-project-root) src-folder)
-          (concat (projectile-project-root) test-folder)))
-
-(defun split-per-line (text)
-  (split-string text "\n"))
-
-(defun list-all-files-in-project (src-folder test-folder)
-    (->>
-     (construct-find-command src-folder test-folder)
-     (shell-command-to-string)
-     (string-trim)
-     (split-per-line)
-     (mapcar 'string-trim)
-     (mapcar 'file-truename)))
-
-(defun evaluate-files (paths)
-  (dolist (p paths) (cider-eval-file p)))
-
-(defun my-cider-load-and-test-project ()
-  (interactive)
-  (my-cider-load-and-test-files
-   (list-all-files-in-project "src" "test")))
-
-(defun my-cider-refresh-and-test-project ()
-  (interactive)
-  (cider-refresh)
-  (cider-test-run-project-tests))
-
-(defun my-cider-load-and-test-current ()
-  (interactive)
-  (my-cider-load-and-test-files (list (buffer-file-name))))
-
-(defun my-cider-load-and-test-files (paths)
-  (reset-counters)
-  (setq nr-of-files-to-load (length paths))
-  (evaluate-files paths)
-  (message "Loading and testing all files in project..."))
-
-(defun test-on-save ()
-  (interactive)
-  (when (member major-mode '(clojurec-mode clojure-mode))
-    (my-cider-refresh-and-test-project)))
-
-(add-hook 'cider-file-loaded-hook 'maybe-run-tests)
-(add-hook 'after-save-hook 'test-on-save)
-(push '("*cider-test-report*" :width 0.6 :position right) popwin:special-display-config)
+(advice-add 'cider-refresh--handle-response :after #'cider-refresh-hook)
