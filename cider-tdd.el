a7fb4dd4c94498d157ff0810f0691386b870e8ed
Andrej Lamov <andrej.lamov@seal-software.com>
2017-06-06 01:56:13 +0200

    Call on-jsload on refresh

--- cider-tdd.el
+++ cider-tdd.el
@@ -1,9 +1,26 @@
+(defun eval-in-cljs-repl (expr)
+  (with-current-buffer (cider-current-repl-buffer "cljs")
+    (insert expr)
+    (cider-repl-return)))
+
+(defun eval-jsload ()
+  (let* ((get-jsload-expr
+          "(-> (figwheel-sidecar.system/fetch-config) :data :all-builds first :figwheel :on-jsload)")
+          (response (cider-nrepl-sync-request:eval get-jsload-expr)))
+    (nrepl-dbind-response response
+        (status id ns session value changed-ns repl-type)
+      (let* ((fun-name (nth 1 (split-string value "\"")))
+             (expr     (concat "(" fun-name ")")))
+        (eval-in-cljs-repl expr)
+        ))))
+
 (defun my-cider-refresh-hook (response log-buffer)
   (nrepl-dbind-response response (out err reloading status error error-ns after before)
     (if (equal '("ok") status)
         (progn
           (cider-sync-request:ns-load-all)
           (cider-test-run-project-tests)
+          (eval-jsload)
           ))))
 
 (advice-add 'cider-refresh--handle-response :after #'my-cider-refresh-hook)
