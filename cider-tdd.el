959f659ed8a3d73960a0994fce72f8d7e4272d5f
Andrej Lamov <andrej.lamov@seal-software.com>
2017-06-06 12:39:55 +0200

    Prefix all functions with "ctdd-" and reformat

--- cider-tdd.el
+++ cider-tdd.el
@@ -1,50 +1,52 @@
 ;;;; Helper functions
-(defun refresh-response-is-ok (response)
-  (nrepl-dbind-response response (out err reloading status error error-ns after before)
-      (equal '("ok") status)))
 
-(defun eval-response-get-return-value (response)
+(defun ctdd-refresh-response-is-ok (response)
+  (nrepl-dbind-response response
+      (out err reloading status error error-ns after before)
+    (equal '("ok") status)))
+
+(defun ctdd-eval-response-get-return-value (response)
   (nrepl-dbind-response response
       (status id ns session value changed-ns repl-type)
     value))
 
-(defun remove-surrounding-quotes (e)
+(defun ctdd-remove-surrounding-quotes (e)
   (nth 1 (split-string e "\"")))
 
-(defun add-surrounding-parentheses (e)
+(defun ctdd-add-surrounding-parentheses (e)
   (concat "(" e ")"))
 
-(defun reformat (e)
+(defun ctdd-reformat (e)
   (-> e
-      (remove-surrounding-quotes)
-      (add-surrounding-parentheses)))
+      (ctdd-remove-surrounding-quotes)
+      (ctdd-add-surrounding-parentheses)))
 
-(defun save-if-file-buffer (&optional args)
+(defun ctdd-save-if-file-buffer (&optional args)
   (when (not (buffer-file-name))
     (save-buffer)))
 
 ;;;; Core
 
-(defun eval-in-cljs-repl (expr)
+(defun ctdd-eval-in-cljs-repl (expr)
   (with-current-buffer (cider-current-repl-buffer "cljs")
     (end-of-buffer)
     (insert expr)
     (cider-repl-return)))
 
-(defun eval-jsload ()
+(defun ctdd-eval-jsload ()
   (-> "(-> (figwheel-sidecar.system/fetch-config) :data :all-builds first :figwheel :on-jsload)"
        (cider-nrepl-sync-request:eval)
-       (eval-response-get-return-value)
-       (reformat)
-       (eval-in-cljs-repl)))
+       (ctdd-eval-response-get-return-value)
+       (ctdd-reformat)
+       (ctdd-eval-in-cljs-repl)))
 
-(defun eval-test-run (response log-buffer)
-  (when (refresh-response-is-ok response)
+(defun ctdd-eval-test-run (response log-buffer)
+  (when (ctdd-refresh-response-is-ok response)
     (cider-sync-request:ns-load-all)
-    (eval-jsload)
+    (ctdd-eval-jsload)
     (cider-test-run-project-tests)))
 
 ;;;; Hook
 
-(advice-add 'cider-refresh--handle-response :after #'eval-test-run)
-(advice-add 'cider-refresh :before #'save-if-file-buffer)
+(advice-add 'cider-refresh--handle-response :after #'ctdd-eval-test-run)
+(advice-add 'cider-refresh :before #'ctdd-save-if-file-buffer)
